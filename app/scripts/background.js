10cca6eeedaa771d76e1f5f26e2ee44f03357663
Joel Kuntz <joel.kuntz.92@gmail.com>
2015-06-01 23:30:07 -0300

    Initial setup of extension

---
+++ app/scripts/background.js
@@ -0,0 +1,35 @@
+'use strict';
+
+chrome.runtime.onInstalled.addListener(function (details) {
+    console.log('previousVersion', details.previousVersion);
+});
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    // console.log('onUpdate tab', tabId, changeInfo, tab);
+
+    var urlObject = url.parse(tab.url);
+    var pagesRegex = /.*?\.github\.io/i;
+    var isPage = pagesRegex.test(urlObject.host);
+    // console.log('is page', isPage, urlObject);
+
+    if (isPage){
+        chrome.pageAction.show(tabId);
+        var userRegex = /^(.*)?\.github\.io/i;
+        var user = userRegex.exec(urlObject.host);
+        var repoRegex = /^\/(.*?)\/.*/i;
+        var repo = repoRegex.exec(urlObject.path);
+        console.log({'username': user, "repo": repo});
+        if (user && repo) {
+            chrome.pageAction.setTitle({
+                tabId: tabId,
+                title: "github.com/"+user[1]+"/"+repo[1]
+            });
+        }
+        else if (user && !repo) {
+            chrome.pageAction.setTitle({
+                tabId: tabId,
+                title: "github.com/"+user[1]+"/"+user[1]+".github.io"
+            });
+        }
+    }
+});
