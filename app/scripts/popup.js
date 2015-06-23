b07ea0b1509b7333f65f018cf313f19e4ca7e1d4
Johann-S <johann.servoire@gmail.com>
2015-06-23 09:10:45 +0200

    Fix indentation

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -3,9 +3,9 @@ var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-63782941-1']);
 
 (function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = 'https://ssl.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = 'https://ssl.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
 //--- End Google Analytics
 
